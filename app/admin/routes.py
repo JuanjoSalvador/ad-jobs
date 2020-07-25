5dddc45c7d5131dca715340ddfa6a6670bd92695
Juanjo Salvador <juanjosalvador@netc.eu>
2020-07-25 22:40:35 +0200

    Panel de contro, pantalla de login, y correcciones menores.

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -3,4 +3,8 @@ from . import admin_bp
 
 @admin_bp.route('/login',methods=['GET', 'POST'])
 def login():
-    return render_template("admin.jinja")
+    return render_template("login.jinja")
+
+@admin_bp.route('/admin',methods=['GET'])
+def admin():
+    return render_template("admin.jinja", hide_footer = True)
